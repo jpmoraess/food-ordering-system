df124dc3c1cb4c958a9f8240009fd60e7054fa10
moraesit <moraess.it@gmail.com>
2022-06-02 15:07:42 -0300

    Implementing Order Application Service - DTO Classes

---
+++ order-service/order-domain/order-application-service/src/main/java/br/com/food/ordering/system/order/service/domain/dto/create/CreateOrderResponse.java
@@ -0,0 +1,21 @@
+package br.com.food.ordering.system.order.service.domain.dto.create;
+
+import br.com.food.ordering.system.order.service.domain.valueobject.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CreateOrderResponse {
+    @NotNull
+    private final UUID orderTrackingId;
+    @NotNull
+    private final OrderStatus orderStatus;
+    @NotNull
+    private final String message;
+}
---
+++ order-service/order-domain/order-application-service/src/main/java/br/com/food/ordering/system/order/service/domain/dto/message/RestaurantApprovalResponse.java
@@ -0,0 +1,22 @@
+package br.com.food.ordering.system.order.service.domain.dto.message;
+
+import br.com.food.ordering.system.order.service.domain.valueobject.OrderApprovalStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class RestaurantApprovalResponse {
+    private String id;
+    private String sagaId;
+    private String orderId;
+    private String restaurantId;
+    private Instant createdAt;
+    private OrderApprovalStatus orderApprovalStatus;
+    private List<String> failureMessages;
+}
