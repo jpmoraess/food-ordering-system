e5c3eaa6e1b8f1ee9f6cd88dbe0225cae694aba7
moraesit <moraess.it@gmail.com>
2022-06-02 13:53:23 -0300

    Adding state changing methods to Order Entity

---
+++ order-service/order-domain/order-domain-core/src/main/java/br/com/food/ordering/system/order/service/domain/entity/Customer.java
@@ -0,0 +1,6 @@
+package br.com.food.ordering.system.order.service.domain.entity;
+
+import br.com.food.ordering.system.order.service.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId>{
+}
