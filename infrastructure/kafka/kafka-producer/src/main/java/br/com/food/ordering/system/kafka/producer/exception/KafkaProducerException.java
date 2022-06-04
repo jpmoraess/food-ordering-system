dbd4edf78754835e7095b3ff18debfd355c1b5f6
jpmoraess <moraess.it@gmail.com>
2022-06-04 19:50:56 -0300

    Implementing Kafka producer generic module

---
+++ infrastructure/kafka/kafka-producer/src/main/java/br/com/food/ordering/system/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,7 @@
+package br.com.food.ordering.system.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException {
+    public KafkaProducerException(String message) {
+        super(message);
+    }
+}
