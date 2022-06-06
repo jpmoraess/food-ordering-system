e2d537d83c1d4427ddd73fd4eb0f195991c28226
jpmoraess <moraess.it@gmail.com>
2022-06-06 15:13:39 -0300

    Container module: Adding Spring Boot Starter Class and Config

---
+++ order-service/order-container/src/main/java/br/com/food/ordering/system/order/service/domain/BeanConfiguration.java
@@ -0,0 +1,13 @@
+package br.com.food.ordering.system.order.service.domain;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public OrderDomainService orderDomainService() {
+        return new OrderDomainServiceImpl();
+    }
+}
