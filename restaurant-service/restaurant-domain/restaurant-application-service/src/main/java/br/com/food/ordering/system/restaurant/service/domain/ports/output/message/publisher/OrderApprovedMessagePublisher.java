b7d64f83e04202edbb7b54f70e6091ceb26bc83b
jpmoraess <moraess.it@gmail.com>
2022-06-10 23:02:20 -0300

    Application Service domain module: Implementing Input Ports

--- restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/br/com/food/ordering/system/restaurant/service/domain/ports/output/message/publisher/OrderApprovedMessagePublisher.java
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/br/com/food/ordering/system/restaurant/service/domain/ports/output/message/publisher/OrderApprovedMessagePublisher.java
@@ -1,7 +1,7 @@
 package br.com.food.ordering.system.restaurant.service.domain.ports.output.message.publisher;
 
 import br.com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
-import br.com.food.ordering.system.restaurant.service.domain.event.OrderApprovalEvent;
+import br.com.food.ordering.system.restaurant.service.domain.event.OrderApprovedEvent;
 
-public interface OrderApprovedMessagePublisher extends DomainEventPublisher<OrderApprovalEvent> {
+public interface OrderApprovedMessagePublisher extends DomainEventPublisher<OrderApprovedEvent> {
 }
